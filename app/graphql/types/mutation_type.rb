b42b3630e1e5466a6d22702d57f12d4c1892850f
Emmanuel Etti <emmanueletti@gmail.com>
2022-05-19 16:21:37 -0700

    update

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,12 @@ module Types
   class MutationType < Types::BaseObject
     # REMEMBER: we dont need to use require because of rails magic aka we are in the app folder
     # but normally for ruby code we could need to
+    
+    # in this implementation, mutations is wierd in that they get thier own
+    # namespace
+    # (TANGENT: REMEMBER THAT: in rails folders are used to designate and infer namespaces)
+    # queryTypes dont get thier own special namespace and are lumped in the Types namespace with
+    # everything else
     field :create_user, mutation: Mutations::CreateUser
   end
 end
